d4c6dede8f6dcf67afdf09a51ef62537fd9da9c2
lion-rion <lionact08@gmail.com>
2022-08-19 16:36:07 +0900

    ✅Update : header

--- src/config/links.tsx
+++ src/config/links.tsx
@@ -3,8 +3,10 @@ import { FiTag } from 'react-icons/fi';
 import { LinkType } from '@/types/link';
 //import { GrAchievement } from 'react-icons/gr';
 import { CgNotes } from 'react-icons/cg';
+import { AiOutlineHome } from 'react-icons/ai';
 
 export const links: LinkType[] = [
+  { name: 'Home', href: '/', icon: <AiOutlineHome /> },
   { name: 'About', href: '/about', icon: <AiOutlineInfoCircle /> },
   { name: 'Blog', href: '/posts/page/1/', icon: <CgNotes /> },
   { name: 'Tags', href: '/tags', icon: <FiTag /> },
