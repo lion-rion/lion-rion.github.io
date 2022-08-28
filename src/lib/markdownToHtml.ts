fef6d669737f197d94bb7f4624a239a243ff0d66
lion-rion <lionact08@gmail.com>
2022-08-28 12:58:17 +0900

    ✅ add : aseets

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -5,9 +5,14 @@ import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
+import remark from 'remark';
+import html from 'remark-html';
+import gfm from 'remark-gfm';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
+    .use(html)
+    .use(gfm)
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypePrism)
